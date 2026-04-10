e1c1456448763f6bdaba66943934daac9fbb7085
Kuredew <burrohmanhabib35@gmail.com>
2026-04-10 16:53:52 +0700

    feat: add http logging

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import { indexRouter } from './routes'
 import { SERVER_PORT } from '@/config'
+import morgan from 'morgan'
 import cors from 'cors'
 
 const app = express()
 
+app.use(morgan('combined'))
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/v1', indexRouter)
 
-app.listen(SERVER_PORT, () => console.log('server running on port 3000.'))
+app.listen(SERVER_PORT, () => console.log(`[main] server running on port ${SERVER_PORT}.`))
