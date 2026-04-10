8e44ee2fdb786362cf58aaed791c51ce642615de
Kuredew <burrohmanhabib35@gmail.com>
2026-04-10 20:43:24 +0700

    refactor: add support for nodejs environment (esbuild)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = Bun.env.BASE_URL
-export const SERVER_PORT = Bun.env.SERVER_PORT
+export const BASE_URL = process.env.BASE_URL
+export const SERVER_PORT = process.env.SERVER_PORT
 
-export const DB_NAME = Bun.env.DB_NAME
-export const DB_USER = Bun.env.DB_USER
-export const DB_PASS = Bun.env.DB_PASS
+export const DB_NAME = process.env.DB_NAME
+export const DB_USER = process.env.DB_USER
+export const DB_PASS = process.env.DB_PASS
